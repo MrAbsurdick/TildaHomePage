1089376ab1df387db9385c270fd1baadb37e56ca
MrAbsurdick <105416118+MrAbsurdick@users.noreply.github.com>
2025-04-09 00:16:03 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -11,19 +11,12 @@ const Config = {
                     "color_fg_subtitle": "#e66832", 
                     "color_fg_text": "#DDD", 
                     "color_fg_subtext": "#9BA4B5",
-                    
-                    "color_bg_first": "#152327AA",
-                    "color_bg_second": "#152327BB", 
-                    "color_fg_title": "#ffcf89", 
-                    "color_fg_subtitle": "#3D8D7A", 
-                    "color_fg_text": "#DDD", 
-                    "color_fg_subtext": "#bed4cd",
 
-                    "border_radius": "8px",
+                    "border_radius": "4px",
                     "bookmark_icons": "block",
                     
 
-                    "background_image": "https://r4.wallpaperflare.com/wallpaper/158/888/959/nature-landscape-trees-forest-wallpaper-9be68ced83717f69a5940bb99d8c4c00.jpg",
+                    "background_image": "https://c1.wallpaperflare.com/path/427/745/192/notebook-natural-laptop-macbook-497500668a927f46aa19fafb668d8702.jpg",
                     
                     "background_blur": "0px",
                     "panel_blur": "15px",
